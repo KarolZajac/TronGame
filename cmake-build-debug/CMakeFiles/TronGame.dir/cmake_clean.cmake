ad6bb11435c1385561cd4c746cad5ea5aaea37f2
DESKTOP-5COLD3J\Karol <karol.zajac30@gmail.com>
2021-06-10 00:15:06 +0200

    init project

---
+++ cmake-build-debug/CMakeFiles/TronGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TronGame.dir/main.cpp.obj"
+  "TronGame.exe"
+  "TronGame.exe.manifest"
+  "TronGame.pdb"
+  "libTronGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TronGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
